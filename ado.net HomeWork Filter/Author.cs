398fecf102c9ad5f86a77828f959317ed377b0c6
DELL <DELL@DESKTOP-V3Q0TUL>
2023-06-11 21:54:35 +0400

    Add project files.

---
+++ ado.net HomeWork Filter/Author.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ado.net_HomeWork_Filter
+{
+    public partial class Author
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; } = null!;
+
+        public string LastName { get; set; } = null!;
+
+        public virtual ICollection<Book> Books { get; set; } = new List<Book>();
+    }
+}
---
+++ ado.net HomeWork Filter/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ado.net_HomeWork_Filter
+{
+    public partial class Category
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public virtual ICollection<Book> Books { get; set; } = new List<Book>();
+    }
+}
